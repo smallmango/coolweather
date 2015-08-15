4813bc684c95d035aae2d243df2c7f4451c630dd
Mango <634886967@qq.com>
2015-08-15 21:56:32 +0800

    First commit.

---
+++ app/src/androidTest/java/com/mango/coolweather/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.mango.coolweather;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
